0fed4a854640c37d63744e91340a831148913f2c
Ubuntu <ubuntu@ip-172-31-74-87.ec2.internal>
2022-03-31 03:38:59 +0000

    added files

---
+++ Surface-Area-And-Volume-of-a-rectanglar-prisim.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Created by: Jedidiah
+# Created on: Mar 17 2022
+# this program takes user input
+# and calculates the surface area 
+# and volume of a rectanglar prisim 
+import math 
+
+
+def main():
+    # input
+    print("This Program will calculate the surface area")
+    print("and volume of a rectanglar prisim")
+    print("")
+    print("please enter the length below")
+    length = int(input("Enter the length (mm): "))
+    print("")
+    print("please enter the width  below")
+    width  = int(input("Enter the width  (mm): "))
+    print("")
+    print("please enter the height below")
+    height = int(input("Enter the height (mm): "))
+    print("")
